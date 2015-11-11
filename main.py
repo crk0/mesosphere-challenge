d31ad50a8f9ed1c89cf5dd65ad626e7914d60145
Chinmay Kamat <chinmay.kamat13@imperial.ac.uk>
2015-11-11 16:00:46 +0000

    Added ElevatorControlSystem class with initial implementation of status(), update() and pickup()

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ class Elevator():
 		self._dir 			= 0
 
 	def status(self):
-		# Return triple representing state of elevator,
-		# state(id, current_floor, goal_floor)
+		''' Return triple representing state of elevator,
+			state(id, current_floor, goal_floor)
+		'''
 		return (self._id, self._currentFloor, self._goalFloor)
 
 	def update(self, currentFloor, goalFloors):
+		''' Update the state of the elevator
+		'''
 		# update current floor
 		self._currentFloor = currentFloor
 
@@ -23,3 +26,33 @@ class Elevator():
 
 		# add the new goal floors
 		self._goalFloor.union(set(goalFloors))
+
+class ElevatorControlSystem():
+	def __init__(self, numElevators):
+		# initialise numElevators instances of Elevator()
+		self._elevators 	 = [Elevator(id) for id in range(0,numElevators)]
+		self._pickupRequests = set()
+
+	def status(self):
+		''' Return a list of all states of the elevators 
+		'''
+		return [elevator.status() for elevator in self._elevators]
+
+	def update(self, id, currentFloor, goalFloors):
+		''' Update the state of elevator with ID=id
+		'''
+		# Retrieve elevator with correct id
+		curElevator
+		for elevator in self._elevators:
+			if elevator._id = id:
+				curElevator = elevator
+				break;
+
+		curElevator.update(currentFloor, goalFloors)
+
+	def pickup(self, pickupFloor, direction):
+		''' Add pickup request to pickupRequests queue.
+			A pickup request is represented as a tuple 
+			consisting of the pickupFloor and direction
+		'''
+		self._pickupRequests.add((pickupFloor, direction))
